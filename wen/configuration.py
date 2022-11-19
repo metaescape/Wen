5d96f0184528572d1fa4784576036b4aa0b13d7a
mE <metaescape@foxmail.com>
2022-11-19 15:14:24 +0800

    support org inline latex acm hint

---
+++ wen/configuration.py
@@ -0,0 +1,42 @@
+import os
+import json
+import os.path as osp
+
+CONFIG_DIR = osp.expanduser("~/.config/wen/")
+
+
+class WenConfig:
+    def __init__(self):
+        self.setting = self.read_config()
+        self.latex_table = self.read_latex_table()
+        print(self.latex_table)
+
+    def read_config(self):
+        if not os.path.exists(CONFIG_DIR):
+            os.mkdir(CONFIG_DIR)
+        settings_path = os.path.join(CONFIG_DIR, "settings.json")
+        if os.path.exists(settings_path):
+            with open(settings_path, "r") as f:
+                settings = json.load(f)
+        return settings
+
+    # def read_rime_vocab(settings):
+    #     vocab_paths = settings["vocabularies"]
+    #     vocabs = []
+    #     for path in vocab_paths["rime"]:
+    #         vocabs.extends(read_rime_vocab(path))
+    #     return vocabs
+
+    def read_latex_table(self):
+        if "latex.table.path" not in self.setting:
+            print("latex table file not set")
+            return []
+        path = osp.expanduser(self.setting["latex.table.path"])
+        if not os.path.exists(path):
+            print("latex table file not find")
+            return []
+        table = []
+        with open(path) as f:
+            for line in f.readlines():
+                table.append(line.strip().split())
+        return table
--- wen/server.py
+++ wen/server.py
@@ -1,68 +1,100 @@
 #!/usr/bin/env python
 from pygls.capabilities import COMPLETION
-from pygls.lsp.methods import (COMPLETION, TEXT_DOCUMENT_DID_CHANGE,
-                               TEXT_DOCUMENT_DID_SAVE,
-                               TEXT_DOCUMENT_DID_CLOSE, TEXT_DOCUMENT_DID_OPEN, 
-                               TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL)
-from pygls.lsp.types import (CompletionItem, CompletionList, CompletionOptions,
-                             CompletionParams, ConfigurationItem, TextEdit,
-                             ConfigurationParams, Diagnostic, 
-                             DidChangeTextDocumentParams,
-                             DidSaveTextDocumentParams,
-                             DidCloseTextDocumentParams,
-                             DidOpenTextDocumentParams, MessageType, Position,
-                             Range, Registration, RegistrationParams)
+from pygls.lsp.methods import (
+    COMPLETION,
+    TEXT_DOCUMENT_DID_CHANGE,
+    TEXT_DOCUMENT_DID_SAVE,
+    TEXT_DOCUMENT_DID_CLOSE,
+    TEXT_DOCUMENT_DID_OPEN,
+    TEXT_DOCUMENT_SEMANTIC_TOKENS_FULL,
+)
+from pygls.lsp.types import (
+    CompletionItem,
+    CompletionList,
+    CompletionOptions,
+    CompletionParams,
+    ConfigurationItem,
+    TextEdit,
+    ConfigurationParams,
+    Diagnostic,
+    DidChangeTextDocumentParams,
+    DidSaveTextDocumentParams,
+    DidCloseTextDocumentParams,
+    DidOpenTextDocumentParams,
+    MessageType,
+    Position,
+    Range,
+    Registration,
+    RegistrationParams,
+)
 from pygls.server import LanguageServer
-from pygls.lsp import CompletionItem, CompletionList, CompletionOptions,\
-    CompletionParams
+from pygls.lsp import (
+    CompletionItem,
+    CompletionList,
+    CompletionOptions,
+    CompletionParams,
+)
 import logging
 import os
 import sys
 import re
-proj_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
-sys.path.append(proj_dir) 
+import json
+from configuration import WenConfig
+
+proj_dir = os.path.dirname(os.path.dirname(os.path.realpath(sys.argv[0])))
+sys.path.append(proj_dir)
+
 
 DEBUG = True
-use_godtian = True
-use_pinyin_initial = True
+use_godtian = False
+use_pinyin_initial = False
+
 
 try:
     import jieba
     import jieba.posseg as pseg
+
     jieba.enable_parallel()
+    from pypinyin import lazy_pinyin
 except:
     use_pinyin_initial = False
-
-    
 
 if use_godtian:
     im_dir = os.path.join(proj_dir, "./completion")
     os.chdir(im_dir)
-    sys.path.append(im_dir) 
+    sys.path.append(im_dir)
     from completion import GodTian_Pinyin as gp
+
     godtian = gp.GodTian_Pinyin()
 else:
     godtian = False
+
 
 def debug_logger():
     logger = logging.getLogger("pinyin")
     logger.setLevel(logging.DEBUG)
     # create file handler which logs even debug messages
-    fh = logging.FileHandler('/tmp/wen.log')
+    fh = logging.FileHandler("/tmp/wen.log")
     fh.setLevel(logging.DEBUG)
     # create formatter and add it to the handlers
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     fh.setFormatter(formatter)
     # add the handlers to the logger
     logger.addHandler(fh)
     return logger
+
 
 logger = debug_logger()
 
 words = set(["中文语言服务", "wenls", "Metaescape"])
 
 # TODO: use \' as pinyin separator
-RE_START_WORD = re.compile('[\'A-Za-z_0-9\/]*$')
+RE_START_WORD = re.compile("['A-Za-z_0-9\/]*$")
+
+wen_cfg = WenConfig()
+
 
 def generate_cands_from_godtian(prefix, context=None):
     """
@@ -87,6 +119,20 @@ def items_from_map(symbol_map, pos):
     return items
 
 
+def items_from_triple_map(triples, pos):
+    """
+    mainly for latex
+    """
+    items = []
+    for prefix, show, insert in triples:
+        text_edit = TextEdit(range=Range(start=pos, end=pos), new_text=show)
+        item = CompletionItem(label=prefix)
+        item.text_edit = text_edit
+        item.insert_text = insert
+        items.append(item)
+    return items
+
+
 def items_from_prefix_cands(prefix, cands, pos):
     items = []
     for cand in cands:
@@ -95,25 +141,27 @@ def items_from_prefix_cands(prefix, cands, pos):
         item.text_edit = text_edit
         items.append(item)
     return items
-        
+
 
 def is_all_chinese(strs):
     for _char in strs:
-        if not '\u4e00' <= _char <= '\u9fa5':
+        if not "\u4e00" <= _char <= "\u9fa5":
             return False
     return True
 
+
 def both_noun(posseg_pair):
-    a, b  = posseg_pair
+    a, b = posseg_pair
     return a.flag.startswith("n") and b.flag.startswith("n")
 
+
 def cat_posseg_pair(posseg_pair):
-    a, b  = posseg_pair
+    a, b = posseg_pair
     return "".join((a.word, b.word))
-    
+
 
 def _update_words(ls, params):
-    ls.show_message_log('Updating words...')
+    ls.show_message_log("Updating words...")
     text_doc = ls.workspace.get_document(params.text_document.uri)
     source = text_doc.source
     for line in source.split("\n"):
@@ -123,7 +171,6 @@ def _update_words(ls, params):
         bi_words = [cat_posseg_pair(bi) for bi in bigram]
         words.update(bi_words)
 
-from pypinyin import lazy_pinyin
 
 def generate_pinyin_map_from_words(words):
     pinyin_map = {}
@@ -132,6 +179,42 @@ def generate_pinyin_map_from_words(words):
         initials = "".join([ele[0] for ele in pinyin])
         pinyin_map[initials] = word
     return pinyin_map
+
+
+latex_env_pair = {
+    r"\(": r"\)",
+    r"\[": r"\]",
+    r"\begin": r"\end",
+}
+
+
+def latex_open_left(text):
+    for left, right in latex_env_pair.items():
+        left_idx = text.rfind(left)
+        if left_idx >= 0:
+            right_idx = text.rfind(right)
+            if right_idx < left_idx:
+                return left
+
+
+def latex_open_right(text, left, right):
+    right_idx = text.find(right)
+    if right_idx >= 0:
+        left_idx = text.find(left)
+        if right_idx > left_idx:
+            return right
+
+
+def in_latex_env(doc, pos):
+    offset = doc.offset_at_position(pos)
+    text_before = doc.source[:offset]
+    text_after = doc.source[offset:]
+    left = latex_open_left(text_before)
+    if left:
+        right = latex_env_pair[left]
+        return latex_open_right(text_after, left, right) is not None
+    return False
+
 
 server = LanguageServer()
 
@@ -143,31 +226,40 @@ def completions(params: CompletionParams):
     doc = server.workspace.get_document(params.text_document.uri)
 
     cur_word = doc.word_at_position(pos, RE_START_WORD)
-    logger.debug(doc)
-    if DEBUG == True:
+    if DEBUG:
         logger.debug(cur_word)
 
     completion_list = CompletionList(is_incomplete=False, items=[])
 
-    user_define_map = dict([
-        ("8alpha",   "α"),
-        ("8beta",   "β"),
-        ("8gamma",   "γ"),
-        ("8Delta",  "Δ"),
-        ("9/check",  "- [ ] ")
-    ])
+    user_define_map = dict(
+        [
+            ("8alpha", "α"),
+            ("8beta", "β"),
+            ("8gamma", "γ"),
+            ("8Delta", "Δ"),
+            ("9/check", "- [ ] "),
+        ]
+    )
 
-    completion_list.add_items(items_from_map(user_define_map, pos))
+    # completion_list.add_items(items_from_map(user_define_map, pos))
+
+    if in_latex_env(doc, pos) and cur_word.isalpha():
+        completion_list.add_items(
+            items_from_triple_map(wen_cfg.latex_table, pos)
+        )
 
     if use_pinyin_initial:
         pinyin_map = generate_pinyin_map_from_words(words)
         completion_list.add_items(items_from_map(pinyin_map, pos))
 
-    if godtian and (cur_word.isalpha() or cur_word.replace("'","").isalpha()): 
+    if godtian and (cur_word.isalpha() or cur_word.replace("'", "").isalpha()):
         pinyin = generate_cands_from_godtian(cur_word)
-        completion_list.add_items(items_from_prefix_cands(cur_word, pinyin, pos))
-    
+        completion_list.add_items(
+            items_from_prefix_cands(cur_word, pinyin, pos)
+        )
+
     return completion_list
+
 
 @server.feature(TEXT_DOCUMENT_DID_SAVE)
 def did_change(ls, params: DidSaveTextDocumentParams):
@@ -178,7 +270,8 @@ def did_change(ls, params: DidSaveTextDocumentParams):
 @server.feature(TEXT_DOCUMENT_DID_OPEN)
 async def did_open(ls, params: DidOpenTextDocumentParams):
     """Text document did open notification."""
-    ls.show_message('Text Document Did Open')
+    ls.show_message("Text Document Did Open")
     _update_words(ls, params)
+
 
 server.start_io()
